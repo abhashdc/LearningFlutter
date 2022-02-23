9fa98b709d572494ae7c93a23ebce4a74a876d86
abhashdc <developer.abhash@gmail.com>
2022-02-23 21:40:39 +0545

    Started some instagram design skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Instagram());
@@ -5,6 +6,71 @@ void main() => runApp(Instagram());
 class Instagram extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Instagram',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Instagram',
+                    style:
+                        TextStyle(fontSize: 25, backgroundColor: Colors.blue),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(color: Colors.green),
+                      child: Icon(
+                        Icons.favorite_border,
+                        color: Colors.black,
+                        size: 26.0,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(color: Colors.red),
+                      child: Icon(
+                        Icons.chat_outlined,
+                        color: Colors.black,
+                        size: 24.0,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.amber,
+                    size: 26.0,
+                  ),
+                  Text('abhash'),
+                  Container(
+                    child: Icon(
+                      Icons.more_vert,
+                      color: Colors.black,
+                      size: 26.0,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
