7b49340ae90c062eb465c1fb4efc65b30947b719
abhashdc <developer.abhash@gmail.com>
2022-02-24 07:54:55 +0545

    This is completion of button

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,159 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(CreateButton());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class CreateButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Practice Buttons'),
+        ),
+        body: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            PracticeTextButton(
+              buttonText: 'Simple button',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            PracticeElevatedButton(
+              buttonText: 'Elevated Button',
             ),
+            PracticeFloatingActionButton(),
+            PracticeIconButton(),
+            PracticeOutlinedButton(
+              buttonText: 'Outlined Button',
+            ),
+            PracticeDropdownButton(),
+            PracticePopupMenuButton(),
           ],
         ),
+        // floatingActionButton: PracticeFloatingActionButton(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class PracticeTextButton extends StatelessWidget {
+  String buttonText;
+  PracticeTextButton({this.buttonText = 'Text Button'});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 20),
+        ),
+        onPressed: () {},
+        child: Text(buttonText),
+      ),
+    );
+  }
+}
+
+class PracticeDropdownButton extends StatefulWidget {
+  @override
+  State<PracticeDropdownButton> createState() => _PracticeDropdownButtonState();
+}
+
+class _PracticeDropdownButtonState extends State<PracticeDropdownButton> {
+  String dropdownValue = 'One';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    ));
+  }
+}
+
+class PracticeElevatedButton extends StatelessWidget {
+  String buttonText;
+  PracticeElevatedButton({this.buttonText = 'ElevatedButton'});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(buttonText),
+      ),
+    );
+  }
+}
+
+class PracticeFloatingActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.navigation),
+      ),
+    );
+  }
+}
+
+class PracticeIconButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: IconButton(
+        icon: const Icon(Icons.volume_up),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class PracticeOutlinedButton extends StatelessWidget {
+  String buttonText;
+  PracticeOutlinedButton({this.buttonText = 'OutlinedButton'});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: OutlinedButton(
+        onPressed: () {},
+        child: Text(buttonText),
+      ),
+    );
+  }
+}
+
+class PracticePopupMenuButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: PopupMenuButton(
+            itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: Text("First"),
+                    value: 1,
+                  ),
+                  PopupMenuItem(
+                    child: Text("Second"),
+                    value: 2,
+                  )
+                ]));
   }
 }
