a9d10c85c3de3f340ea8d28e5bd5c0c56922a870
abhashdc <developer.abhash@gmail.com>
2022-02-24 09:10:35 +0545

    Completed bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -19,26 +19,28 @@ class Instagram extends StatelessWidget {
                 Container(
                   child: Text(
                     'Instagram',
-                    style:
-                        TextStyle(fontSize: 25, backgroundColor: Colors.blue),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
                 Row(
                   children: <Widget>[
                     Container(
-                      decoration: BoxDecoration(color: Colors.green),
-                      child: Icon(
-                        Icons.favorite_border,
+                      margin: EdgeInsets.only(top: 5),
+                      // decoration: BoxDecoration(color: Colors.green),
+                      child: IconButton(
+                        icon: Icon(Icons.favorite_border),
+                        iconSize: 25,
                         color: Colors.black,
-                        size: 26.0,
+                        onPressed: () {},
                       ),
                     ),
                     Container(
-                      decoration: BoxDecoration(color: Colors.red),
+                      margin: EdgeInsets.only(left: 20, top: 10),
+                      // decoration: BoxDecoration(color: Colors.red),
                       child: Icon(
                         Icons.chat_outlined,
                         color: Colors.black,
-                        size: 24.0,
+                        size: 25.0,
                       ),
                     )
                   ],
@@ -47,26 +49,74 @@ class Instagram extends StatelessWidget {
             ),
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.account_circle_rounded,
-                    color: Colors.amber,
-                    size: 26.0,
-                  ),
-                  Text('abhash'),
-                  Container(
-                    child: Icon(
-                      Icons.more_vert,
-                      color: Colors.black,
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.account_circle_rounded,
+                      color: Colors.amber,
                       size: 26.0,
                     ),
-                  ),
-                ],
+                    Text('abhash'),
+                    Container(
+                      child: Icon(
+                        Icons.more_vert,
+                        color: Colors.black,
+                        size: 26.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false, //Disable showing labels
+          showUnselectedLabels: false, //Disable showing labels
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
               ),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              label: 'Search',
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              label: 'Add',
+              icon: Icon(
+                Icons.add_box_outlined,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              label: 'Shop',
+              icon: Icon(
+                Icons.local_mall_outlined,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              label: 'Profile',
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
             )
           ],
         ),
