cc0691182c4084124b8ea90667b1b147cd69d8d8
abhashdc <developer.abhash@gmail.com>
2022-03-02 11:59:50 +0545

    Trying to change tab bar color

--- lib/main.dart
+++ lib/main.dart
@@ -2,245 +2,77 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: MainScreen()));
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CustomListView()),
-                  );
+    // int indexOfTab = DefaultTabController.of(context)!.index;
+    int indexOfTab = 1;
+    List<Color> listOfColors = [
+      Colors.indigo,
+      Colors.lightGreen,
+      Colors.redAccent
+    ];
+    return MaterialApp(
+        home: DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Make tab bar indicator dynamic'),
+          bottom: TabBar(
+            onTap: (index) {
+              setState(
+                () {
+                  indexOfTab = index;
+                  // print(indexOfTab);
                 },
-                child: Text('Click to see List View'),
+              );
+            },
+            indicatorColor: listOfColors[indexOfTab],
+            unselectedLabelColor: Colors.red,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(
+                color: Colors.red,
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CustomGridView()),
-                  );
-                },
-                child: Text('Click to see Grid View'),
+            ),
+            automaticIndicatorColorAdjustment: false,
+            indicatorWeight: 5,
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.directions_car,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.directions_transit,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.directions_bike,
+                ),
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class Item {
-  //Modal class
-  String networkImageUrl, name;
-  int quantity;
-  double price;
-
-  Item(
-      {required this.networkImageUrl,
-      required this.name,
-      required this.quantity,
-      required this.price});
-}
-
-class CustomCard extends StatelessWidget {
-  CustomCard({Key? key, required this.item}) : super(key: key);
-  Item item;
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20), // Image border
-            child: SizedBox.fromSize(
-              size: Size.fromRadius(48), // Image radius
-              child: Image.network(item.networkImageUrl, fit: BoxFit.fill),
+        body: TabBarView(
+          children: [
+            Icon(
+              Icons.directions_car,
             ),
-          ),
-          Text("Name: ${item.name}"),
-          Text("Quantity: ${item.quantity}"),
-          Text("Price: ${item.price}"),
-        ],
-      ),
-    );
-  }
-}
-
-class CustomListView extends StatelessWidget {
-  final List<Item> listOfItems = [
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Thats the list view'),
-      ),
-      body: Container(
-        width: deviceWidth,
-        //color: Colors.purpleAccent,
-        child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: listOfItems.length,
-            padding: EdgeInsets.all(deviceWidth * 0.1),
-            itemBuilder: (context, index) {
-              return CustomCard(item: listOfItems[index]);
-            }),
-      ),
-    );
-  }
-}
-
-class CustomGridView extends StatelessWidget {
-  final List<Item> listOfItems = [
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl:
-          'https://media.istockphoto.com/photos/patan-picture-id637696304?k=20&m=637696304&s=612x612&w=0&h=GqmMtggU2LgHWcXPFNxMrZg9dtPzyrnl9ek5oARcI7Y=',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl:
-          'https://media.istockphoto.com/photos/patan-picture-id637696304?k=20&m=637696304&s=612x612&w=0&h=GqmMtggU2LgHWcXPFNxMrZg9dtPzyrnl9ek5oARcI7Y=',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl:
-          'https://media.istockphoto.com/photos/patan-picture-id637696304?k=20&m=637696304&s=612x612&w=0&h=GqmMtggU2LgHWcXPFNxMrZg9dtPzyrnl9ek5oARcI7Y=',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-    Item(
-      name: 'Profile NFT',
-      networkImageUrl: 'https://picsum.photos/200/300?grayscale',
-      price: 2,
-      quantity: 2,
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('This is grid view'),
-      ),
-      body: Center(
-        child: Container(
-          child: GridView.builder(
-            shrinkWrap: true,
-            itemCount: listOfItems.length,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemBuilder: (context, index) {
-              return CustomCard(item: listOfItems[index]);
-            },
-          ),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
