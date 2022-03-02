201862a03fff9a8a05e1b814bdde4bce87e79153
abhashdc <developer.abhash@gmail.com>
2022-03-02 12:51:04 +0545

    This will give border raidus

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        // height: MediaQuery.of(context).size.height,
-        color: Colors.lime,
-        child: Row(
-          
-          children: [
-            Expanded(
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
+      appBar: AppBar(
+        title: Text('Productivity Timer'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height,
+            // color: Colors.lime,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  width: MediaQuery.of(context).size.width * 0.9 / 3,
+                  color: Colors.deepOrange,
+                  child: const Text(
+                    'Work',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  width: MediaQuery.of(context).size.width * 0.9 / 3,
+                  color: Colors.lightGreen,
+                  child: const Text(
+                    'Short Break',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(50)),
+                  width: MediaQuery.of(context).size.width * 0.9 / 3,
+                  color: Colors.green,
+                  child: const Text(
+                    'Long Break',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
             ),
-            Container(
-              width: MediaQuery.of(context).size.width / 3,
-              height: 100,
-              color: Colors.blue,
+          ),
+          Expanded(
+            child: Container(
+              child: Text('Hi'),
             ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.black,
-            )
-          ],
-        ),
+          ),
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  margin: EdgeInsets.all(5),
+                  width: MediaQuery.of(context).size.width * 0.9 / 2,
+                  color: Colors.deepOrange,
+                  child: const Text(
+                    'Stop',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  margin: EdgeInsets.all(5),
+                  width: MediaQuery.of(context).size.width * 0.9 / 2,
+                  color: Colors.green,
+                  child: const Text(
+                    'Restart',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
